2e491d4facc812e34fb9fbdd61d7e2116918d671
alessandro <alessandro.carella.universita@gmail.com>
2025-02-12 20:28:58 +0100

    removed grid lines from PCA plot

--- pca visualization prototype/index.js
+++ pca visualization prototype/index.js
@@ -40,27 +40,19 @@ function createZoom(svg, g, margin, width, height) {
 function createAxes(g, x, y, margin, width, height) {
     // X-axis
     g.append("g")
-        .attr("class", "grid")
+        .attr("class", "x-axis")
         .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(
-            d3
-                .axisBottom(x)
-                .ticks(10)
-                .tickSize(-height + margin.top + margin.bottom)
-        )
-        .call((g) => g.select(".domain").remove());
+        .call(d3.axisBottom(x).ticks(10))
+        .call((g) => g.select(".domain").remove())  // Remove axis line
+        .call((g) => g.selectAll(".tick line").remove());  // Remove tick lines
 
     // Y-axis
     g.append("g")
-        .attr("class", "grid")
+        .attr("class", "y-axis")
         .attr("transform", `translate(${margin.left},0)`)
-        .call(
-            d3
-                .axisLeft(y)
-                .ticks(10)
-                .tickSize(-width + margin.left + margin.right)
-        )
-        .call((g) => g.select(".domain").remove());
+        .call(d3.axisLeft(y).ticks(10))
+        .call((g) => g.select(".domain").remove())  // Remove axis line
+        .call((g) => g.selectAll(".tick line").remove());  // Remove tick lines
 }
 
 // Create the Voronoi regions for the decision boundaries
